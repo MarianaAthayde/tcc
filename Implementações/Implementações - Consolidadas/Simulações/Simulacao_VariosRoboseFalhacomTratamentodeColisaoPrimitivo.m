18a9f9b2081750124ee27f286125a8ac848d9b41
MarianaAthayde <soda_nana182@hotmail.com>
2015-12-04 01:38:29 -0200

    Repositório reorganizado e consolidado

---
+++ Implementações/Implementações - Consolidadas/Simulações/Simulacao_VariosRoboseFalhacomTratamentodeColisaoPrimitivo.m
@@ -0,0 +1,123 @@
+clear all
+close all
+clc
+
+%Simulação
+%Variáveis do Sistema
+num_robos = 5; %número de robos do sistema
+raio = 0.3; %Raio em torno do alvo - metros
+periodo = 24; %Período desejado
+dist_min = 0.2; %Distancia mínima de colisão
+wd = 2*pi/periodo; 
+vd = wd*raio;
+t_amostral = 25/1000.0;
+amostras = 1000;
+kp = 2;
+kv = 0.5;
+%Posição do Alvo
+x_alvo = 0.0;
+y_alvo = 0.0;
+%Posições Iniciais dos Robos
+%Nota: [Mestre Slave1 Slave2]
+for i = 1:num_robos;
+    xr(1,i) = rand(1)*(-1)^(randi(2,1)); 
+    yr(1,i) = rand(1)*(-1)^(randi(2,1));
+    thetar(1,i) = pi;
+    wr(1,i) = rand(1);
+    vr(1,i) = rand(1);
+end;
+
+qnt_robos(1) = num_robos;
+
+dist = zeros(qnt_robos(1),qnt_robos(1));
+
+for i = 2:amostras
+%Nota: [Mestre Slave1 Slave2]
+qnt_robos(i) = num_robos;
+for j = 0:num_robos-1
+    xd(i,j+1) = x_alvo + raio*cos(i*t_amostral*wd + j*2*pi/num_robos);
+    yd(i,j+1) = y_alvo + raio*sin(i*t_amostral*wd + j*2*pi/num_robos);
+%Calculando Xr e Yr e Thetar dos Robos no instante i    
+    xr(i,j+1) = xr(i-1,j+1) + t_amostral*vr(i-1,j+1)*cos(thetar(i-1,j+1));
+    yr(i,j+1) = yr(i-1,j+1) + t_amostral*vr(i-1,j+1)*sin(thetar(i-1,j+1));
+    thetar(i,j+1) = thetar(i-1,j+1) + t_amostral*wr(i-1,j+1);
+%Calculando Theta desejado apartir dos erros dos Robos no instante i
+    errxd = xd(i,j+1) - xr(i,j+1);
+    erryd = yd(i,j+1) - yr(i,j+1);
+    thetad = atan2(erryd,errxd);
+    errt = thetad - thetar(i,j+1);
+    errt = atan2(sin(errt),cos(errt));
+    wr(i,j+1) = kp*errt;   
+    edist = sqrt(errxd^2+erryd^2);
+    vr(i,j+1) = vd + kv*edist;   
+end;
+
+%Verificando as distÂncias das posições desejadas
+ for k = 1:num_robos
+     for l = k:num_robos
+         if l == k
+             dist(k,l) = 1.0 + dist_min;
+         else
+            errx = xr(i,k) - xr(i,l);
+            erry = yr(i,k) - yr(i,l);
+            dist(k,l) = sqrt(errx^2+erry^2);
+            dist(l,k) = dist(k,l);
+         end
+     end;
+ end;
+ 
+%  if i<=10
+%  sprintf('Dist: %d', i);
+%  dist
+%  end;
+ 
+ %Não está para colidir
+ flag = 0;
+  for k = num_robos:-1:1
+     for l = k:-1:1
+         if dist(k,l) <= dist_min
+             flag = 1;
+         %sprintf('Cheguei 0');
+         end
+     end;
+     if flag == 1
+         vr(i,k) = 0.0;
+         wr(i,k) = 0.0;
+        % sprintf('Cheguei');
+%      else
+%          vr(i,k) = vr(1,k);        
+%          wr(i,k) = wr(1,k);
+     end
+     flag = 0;         
+ end;
+%  if i <= 10
+%  sprintf('Vr: %d', i);
+%  vr
+%  end;
+ 
+    %if i == randi(amostras,1)
+     %   num_robos = 2;
+    %end
+end;
+
+figure;
+t = title('Segundo Problema: Com Medidas para Evitar Colisões','FontSize',24,'FontWeight','bold');
+xl = xlabel('X (metros)','FontSize',22,'FontWeight','demi');
+yl = ylabel('Y (metros)','FontSize',22,'FontWeight','demi');
+
+for i = 1:10:amostras
+    for j = 1:qnt_robos(i)
+       plot(xr(1:i,j),yr(1:i,j),'b');
+       hold on;
+       plot(xr(i,j),yr(i,j),'b*');
+        t = title('Segundo Problema: Com Medidas para Evitar Colisões','FontSize',24,'FontWeight','bold');
+        xl = xlabel('X (metros)','FontSize',22,'FontWeight','demi');
+        yl = ylabel('Y (metros)','FontSize',22,'FontWeight','demi');
+        set(gca,'FontSize',22,'FontWeight','demi');
+       axis equal
+       plot(xd(1:i,j),yd(1:i,j),'r');
+       plot(xd(i,j),yd(i,j),'r*');
+    end;       
+       pause(0.0001);
+       hold off
+end;
---
+++ Implementações/Implementações - Consolidadas/Simulações/Simulacao_VarreduraRedeCentralizada.m
@@ -0,0 +1,75 @@
+clear all;
+close all;
+clc;
+
+%Simulação Varredura em Paralelo
+%[ x | y | v ]
+x(1,:) = [randi(30), randi(30), randi(30), randi(30)];
+%x(1,:) = [-10, 0, 100];
+y(1,:) = [1,1.2,1.4,1.6];
+v(1,:) = [0.0,0.0,0.0,0.0];
+
+amostras = 50;
+t_amostral = 0.25;
+k = 0.5;
+vd = 0.5;
+
+for i = 1:amostras
+%     mx = max(x(i,:));
+%     mn = min(x(i,:)); 
+%     xd = ( max(x(i,:)) + min(x(i,:)) )/2;
+     
+    for j = 1:4
+        
+        if j == 1
+        v(i+1,j) = vd + k * ( (x(i,2) - x(i,j)) + (x(i,3) - x(i,j)) + (x(i,4) - x(i,j)));
+        end;
+        if j == 2
+        v(i+1,j) = vd + k * ( (x(i,1) - x(i,j)));
+        end;
+        if j == 3
+        v(i+1,j) = vd + k * ( (x(i,1) - x(i,j)));
+        end;
+        if j == 4
+        v(i+1,j) = vd + k * ( (x(i,1) - x(i,j)));
+        end;
+        %v(i+1,j) = vd + k * (xd - x(i,j));
+        x(i+1,j) = x(i,j) + v(i+1,j)*cos(0)*t_amostral;
+        y(i+1,j) = y(i,j) + v(i+1,j)*sin(0)*t_amostral; 
+    end;
+    
+end;
+
+figure;
+        t = title('Problema 1 - Rede Centralizada: vd = 0.5m/s','FontSize',24,'FontWeight','bold');
+        xl = xlabel('X (metros)','FontSize',22,'FontWeight','demi');
+        yl = ylabel('Y (metros)','FontSize',22,'FontWeight','demi');
+%pause(10);
+for i = 1:amostras
+        
+    for j = 1:4
+        
+        plot(x(1:i,j),y(1:i,j),'r');
+        t = title('Problema 1 - Rede Centralizada: vd = 0.5m/s','FontSize',24,'FontWeight','bold');
+        xl = xlabel('X (metros)','FontSize',22,'FontWeight','demi');
+        yl = ylabel('Y (metros)','FontSize',22,'FontWeight','demi');
+        %set(l,'FontSize',22,'FontWeight','demi');
+        set(gca,'FontSize',22,'FontWeight','demi');
+        hold on;
+        plot(x(i,j),y(i,j),'r*');    
+     
+    end;
+    pause(0.1);
+    hold off;
+end;
+
+figure;
+
+        plot(x(:,1),'r');
+        t = title('Problema 1 - Rede Centralizada: dxt','FontSize',24,'FontWeight','bold');
+        xl = xlabel('iterações','FontSize',22,'FontWeight','demi');
+        yl = ylabel('X (metros)','FontSize',22,'FontWeight','demi');
+        hold on;
+        plot(x(:,2),'g');
+        plot(x(:,3),'b');
+        plot(x(:,4),'y');
